850be505d03592841b148cfe8d876b5fa1ec333a
Paula_Brites <paula.brites@globant.com>
2020-03-15 23:55:52 -0300

    Arregle el bug de carga de producto
    y tambien de la obtencion de la info de producto

--- src/test/java/com/globant/automation/cyf2020/tests/Main.java
+++ src/test/java/com/globant/automation/cyf2020/tests/Main.java
@@ -17,17 +17,17 @@ public class Main {
 		
 		WebDriver driver = openDriver();
 		driver.manage().window().maximize();
-		driver.navigate().to("https://www.mercadolibre.com.uy/");
+		driver.navigate().to("https://www.mercadolibre.com.uy/"); //navega a la pagina principal
 
 		PrincipalPagina PrincipalPagina = new PrincipalPagina(driver);
-		OfertasSemanales offers = PrincipalPagina.navigateToWeeklyOffers();
+		OfertasSemanales offers = PrincipalPagina.navigateToWeeklyOffers(); // navega a ofertas semanales
 		
+		OfertasSemanales OfertasSemanales = new OfertasSemanales(driver); //creo un objeto de ofertas semanales
 		
-		
-		OfertasSemanales OfertasSemanales = new OfertasSemanales(driver);
-		
-		Producto  Producto = new Producto(driver);
 		Producto products = OfertasSemanales.navigateProducto();
+		
+		//Producto  Producto = new Producto(driver);
+		
 		
         
 		
--- src/test/java/com/globant/automation/cyf2020/tests/OfertasSemanales.java
+++ src/test/java/com/globant/automation/cyf2020/tests/OfertasSemanales.java
@@ -1,5 +1,6 @@
 package com.globant.automation.cyf2020.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,22 +11,26 @@ import com.globant.automation.cyf2020.infrastructure.BasePage;
 
 public class OfertasSemanales extends BasePage {
     
-	@FindBy(xpath="//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class = \"rowItem item highlighted item--grid new to-item\"]")
+	@FindBy(xpath="//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class =\"item__image item__image--grid\"]//div[@class =\"images-viewer\"]")
 	private WebElement primerItem;
 	
-	@FindBy(xpath = "//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class = \"rowItem item highlighted item--grid new to-item\"]//div[@class = \"item__info \"]//span[@class = \"price__fraction\"]")
+	@FindBy(xpath = "//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class = \"item__info \"]//span[@class = \"price__fraction\"]")
 	private WebElement precioPP;
 	
 	@FindBy(xpath="//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//a[@class = \"item__info-link item__js-link \"]//h2[@class = \"item__title list-view-item-title \"]//span[@class = \"main-title\"]")
 	private WebElement nameOfProductPP;
 	
-	@FindBy(xpath="//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class = \"rowItem item highlighted item--grid new to-item\"]//div[@class = \"item__info \"]//div[@class = \"item__discount \"]")
+	@FindBy(xpath="//li[@class = \"results-item highlighted article grid item-info-height-195\"][1]//div[@class = \"item__info \"]//div[@class = \"item__discount \"]")
 	private WebElement porcetagePP;
+	
+	@FindBy(xpath="//h1[\"breadcrumb__title\"]")
+	private WebElement TiendasOficialTITLE;
 	
 
 	 public Producto navigateProducto() {
 		 obtenerDatos();
-   		click(primerItem);
+		 
+		 click(primerItem);
    		return getNextPage(Producto.class);
    	}
 	 
@@ -70,8 +75,9 @@ public class OfertasSemanales extends BasePage {
 		String precio1 = getText(precioPP); 
 		String nombre1 = getText(nameOfProductPP); 
 		String porcentage1 = getText(porcetagePP); 
-		System.out.println();
+		System.out.println(precio1);
 	}
+	
 	
 	
 	
