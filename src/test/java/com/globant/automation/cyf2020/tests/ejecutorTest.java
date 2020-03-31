53708a308f191f7c124345e3751565a8c26a106f
PaulaBritesG <61764228+PaulaBritesG@users.noreply.github.com>
2020-03-31 09:59:06 -0300

    Update ejecutorTest.java

--- src/test/java/com/globant/automation/cyf2020/tests/ejecutorTest.java
+++ src/test/java/com/globant/automation/cyf2020/tests/ejecutorTest.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -33,7 +34,7 @@ private WebDriver driver;
 		driver.get("https://uat.starmeup.com/login.html?continue=aHR0cHM6Ly91YXQuc3Rhcm1ldXAuY29tLw%3D%3D&origin=SMU&logout=true");
 	} 
 	
-	
+	@AfterMethod
 	public void closeDriver() {
 		driver.close();
 	}
