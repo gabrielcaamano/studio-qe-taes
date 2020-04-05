d111f94a9fd040dfa714cdd3f811767ead922f83
PaulaBritesG <61764228+PaulaBritesG@users.noreply.github.com>
2020-04-05 19:31:56 -0300

    Create Equipo.java

---
+++ src/test/java/com/globant/automation/cyf2020/tests/Equipo.java
@@ -0,0 +1,24 @@
+package com.globant.automation.cyf2020.tests;
+
+public enum Equipo {
+
+	BARÇA("FC Barcelona",1), REAL_MADRID("Real Madrid",2),
+	SEVILLA("Sevilla FC",4), VILLAREAL("Villareal",7); 
+	
+	private String nombreClub;
+	private int puestoLiga;
+	
+	private Equipo (String nombreClub, int puestoLiga){
+		this.nombreClub = nombreClub;
+		this.puestoLiga = puestoLiga;
+	}
+
+	public String getNombreClub() {
+		return nombreClub;
+	}
+
+	public int getPuestoLiga() {
+		return puestoLiga;
+	}	
+	
+}
